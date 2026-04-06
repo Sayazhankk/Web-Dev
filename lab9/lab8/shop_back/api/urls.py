2f47c96ba607b62637e6044a237ad0f146e7bf7a
Sayazhankk <sayazhan.zekenova.07@mail.ru>
2026-04-06 19:41:45 +0500

    lab10

--- lab9/lab8/shop_back/api/urls.py
+++ lab9/lab8/shop_back/api/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProductViewSet
 
@@ -6,3 +7,16 @@ router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
 
 urlpatterns = router.urls
+=======
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('products/', ProductListAPIView.as_view()),
+    path('products/<int:product_id>/', ProductDetailAPIView.as_view()),
+
+    path('categories/', CategoryListAPIView.as_view()),
+    path('categories/<int:category_id>/', CategoryDetailAPIView.as_view()),
+    path('categories/<int:category_id>/products/', CategoryProductsAPIView.as_view()),
+]
+>>>>>>> fec67e18 (lab10)
--- lab9/lab8/shop_back/shop_back/urls.py
+++ lab9/lab8/shop_back/shop_back/urls.py
@@ -2,11 +2,19 @@ from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
 
+<<<<<<< HEAD
 def home(request):  
     return HttpResponse("API работает 🚀")
 
 urlpatterns = [
     path('', home),
+=======
+def home(request):
+    return HttpResponse("API is running 🚀")
+
+urlpatterns = [
+    path('', home),  # 👈 add this
+>>>>>>> fec67e18 (lab10)
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
